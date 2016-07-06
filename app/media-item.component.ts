38e8eea342f575c50d0a11375764befcb703cf4b
Justin Schwartzenberger <schwarty@gmail.com>
2016-07-06 00:57:39 -0700

    end of video commit

--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component, Input} from 'angular2/core';
 
 @Component({
     selector: 'media-item',
@@ -6,6 +6,8 @@ import {Component} from 'angular2/core';
     styleUrls: ['app/media-item.component.css']
 })
 export class MediaItemComponent {
+    @Input('mediaItemToWatch') mediaItem;
+    
     onDelete() {
         console.log('deleted');
     }
