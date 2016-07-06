82b76f4cdae53363f7d7f3f389aa9ccdbb1cc82d
Justin Schwartzenberger <schwarty@gmail.com>
2016-07-06 00:51:22 -0700

    end of video commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {MediaItemComponent} from './media-item.component';
 
 @Component({
     selector: 'media-tracker-app',
+    directives: [MediaItemComponent],
     templateUrl: 'app/app.component.html',
     styleUrls: ['app/app.component.css']
 })
