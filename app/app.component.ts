a33513da89066a2f616d0eb949296e3a2f1ea85d
Justin Schwartzenberger <schwarty@Justins-MacBook-Pro.local>
2016-07-16 10:47:54 -0700

    end of video commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,12 @@
 import {Component} from 'angular2/core';
 import {MediaItemListComponent} from './media-item-list.component';
 import {MediaItemFormComponent} from './media-item-form.component';
+import {RouteConfig} from 'angular2/router';
 
+@RouteConfig([
+    { path: '/:medium', component: MediaItemListComponent, name: 'List' },
+    { path: '/add', component: MediaItemFormComponent, name: 'AddMediaItem' }
+])
 @Component({
     selector: 'media-tracker-app',
     directives: [MediaItemListComponent, MediaItemFormComponent],
