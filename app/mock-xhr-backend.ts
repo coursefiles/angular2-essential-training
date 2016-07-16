27d7fd2519c01e1bf744a546cae00117a6babd9c
Justin Schwartzenberger <schwarty@Justins-MacBook-Pro.local>
2016-07-16 10:28:11 -0700

    fix xhr backend get method

--- app/mock-xhr-backend.ts
+++ app/mock-xhr-backend.ts
@@ -12,8 +12,12 @@ export class MockXHRBackend {
             var responseOptions;
             switch (request.method) {
                 case RequestMethod.Get:
-                    if (request.url.indexOf('mediaitems?medium=') >= 0) {
-                        var medium = request.url.split('=')[1];
+                    if (request.url.indexOf('mediaitems?medium=') >= 0 || request.url === 'mediaitems') {
+                        var medium;
+                        if (request.url.indexOf('?') >= 0) {
+                            medium = request.url.split('=')[1];
+                            if (medium === 'undefined') medium = '';
+                        }
                         var mediaItems;
                         if (medium) {
                             mediaItems = this._mediaItems.filter(mediaItem => mediaItem.medium === medium);
