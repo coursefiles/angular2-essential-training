c1dcd4b261af2dfb4723e90b75b62950cdde0885
Justin Schwartzenberger <schwarty@Justins-MacBook-Pro.local>
2016-07-16 10:09:04 -0700

    end of video commit

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,4 +1,5 @@
 import {Component} from 'angular2/core';
+import {ControlGroup, Control} from 'angular2/common';
 
 @Component({
     selector: 'media-item-form',
@@ -6,6 +7,17 @@ import {Component} from 'angular2/core';
     styleUrls: ['app/media-item-form.component.css']
 })
 export class MediaItemFormComponent {
+    form;
+
+    ngOnInit() {
+        this.form = new ControlGroup({
+            'medium': new Control('Movies'),
+            'name': new Control(''),
+            'category': new Control(''),
+            'year': new Control('')
+        });
+    }
+
     onSubmit(mediaItem) {
         console.log(mediaItem);
     }
