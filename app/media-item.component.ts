b72813b5158a24cc915b615de4949a4f871420bd
Justin Schwartzenberger <schwarty@gmail.com>
2016-07-31 13:31:40 -0700

    fix spacing

--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -5,5 +5,4 @@ import { Component } from '@angular/core';
     templateUrl: 'app/media-item.component.html',
     styleUrls: ['app/media-item.component.css']
 })
-export class MediaItemComponent {
-}
+export class MediaItemComponent { }
