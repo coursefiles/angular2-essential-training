5084f576a5fb46aa304306a9778f77423d5ddc13
Justin Schwartzenberger <schwarty@gmail.com>
2016-07-31 13:35:08 -0700

    fix spacing

--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'media-item',
-    templateUrl: 'app/media-item.component.html',
-    styleUrls: ['app/media-item.component.css']
+  selector: 'media-item',
+  templateUrl: 'app/media-item.component.html',
+  styleUrls: ['app/media-item.component.css']
 })
 export class MediaItemComponent {
-    name = 'The Redemption';
+  name = 'The Redemption';
 }
