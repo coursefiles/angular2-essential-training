fb6ec97f5b9609260011d0f8a42a074b35b97cbc
Justin Schwartzenberger <schwarty@gmail.com>
2016-08-02 19:44:53 -0700

    fix input and fix spacing

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,10 +4,9 @@ import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
 
 @Component({
-    selector: 'media-tracker-app',
-    directives: [MediaItemListComponent],
-    templateUrl: 'app/app.component.html',
-    styleUrls: ['app/app.component.css']
+  selector: 'media-tracker-app',
+  directives: [MediaItemListComponent],
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css']
 })
-export class AppComponent {
-}
+export class AppComponent { }
