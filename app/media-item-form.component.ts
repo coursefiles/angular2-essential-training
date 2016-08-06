db3c31be7c981775c8ea93144cba75b5345d8757
Justin Schwartzenberger <schwarty@gmail.com>
2016-08-05 20:16:51 -0700

    fix spacing

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -13,21 +13,21 @@ export class MediaItemFormComponent {
     this.form = new FormGroup({
       medium: new FormControl('Movies'),
       name: new FormControl('', Validators.compose([
-          Validators.required, 
-          Validators.pattern('[\\w\\-\\s\\/]+')
-        ])),
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
       category: new FormControl(''),
       year: new FormControl('', this.yearValidator)
     });
   }
-    
+
   yearValidator(control) {
     if (control.value.trim().length === 0) return null;
     let year = parseInt(control.value);
     let minYear = 1800;
     let maxYear = 2500;
     if (year >= minYear && year <= maxYear) return null;
-    return {'year': { 'min': minYear, 'max': maxYear }};
+    return { 'year': { 'min': minYear, 'max': maxYear } };
   }
 
   onSubmit(mediaItem) {
