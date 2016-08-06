c51b9b87b66424fbc39fb1d132911815ec28d3cd
Justin Schwartzenberger <schwarty@gmail.com>
2016-08-05 19:13:34 -0700

    fix spacing

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'media-item-form',
-    templateUrl: 'app/media-item-form.component.html',
-    styleUrls: ['app/media-item-form.component.css']
+  selector: 'media-item-form',
+  templateUrl: 'app/media-item-form.component.html',
+  styleUrls: ['app/media-item-form.component.css']
 })
 export class MediaItemFormComponent {
 }
