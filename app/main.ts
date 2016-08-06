96a44ebf003db0a0b38680aa7df9051298b7bee1
Justin Schwartzenberger <schwarty@gmail.com>
2016-08-05 19:28:52 -0700

    add media item form usage

--- app/main.ts
+++ app/main.ts
@@ -4,12 +4,14 @@ import { PLATFORM_DIRECTIVES, PLATFORM_PIPES } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
+import { MediaItemFormComponent } from './media-item-form.component';
 import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
 
 let appDirectives = [
   MediaItemComponent,
   MediaItemListComponent,
+  MediaItemFormComponent,
   FavoriteDirective
 ];
 
