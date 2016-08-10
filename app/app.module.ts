ca9c5ac77a750209c65d601906ec29cead171739
Justin Schwartzenberger <schwarty@gmail.com>
2016-08-09 21:15:17 -0700

    update to RC5

---
+++ app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item.component';
+import { MediaItemListComponent } from './media-item-list.component';
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    MediaItemComponent,
+    MediaItemListComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
