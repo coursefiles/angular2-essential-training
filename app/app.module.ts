726306f2654621bc42039dea867324b9c94af35a
Justin Schwartzenberger <schwarty@gmail.com>
2016-08-09 21:44:26 -0700

    update to RC5

---
+++ app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item.component';
+import { MediaItemListComponent } from './media-item-list.component';
+import { MediaItemFormComponent } from './media-item-form.component';
+import { FavoriteDirective } from './favorite.directive';
+import { CategoryListPipe } from './category-list.pipe';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    AppComponent,
+    MediaItemComponent,
+    MediaItemListComponent,
+    MediaItemFormComponent,
+    FavoriteDirective,
+    CategoryListPipe
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
