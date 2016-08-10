bbe6122532b31ddaa5a1b5a2181dedcb2013456f
Justin Schwartzenberger <schwarty@gmail.com>
2016-08-09 21:13:32 -0700

    update to RC5

--- app/main.ts
+++ app/main.ts
@@ -1,19 +1,5 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { PLATFORM_DIRECTIVES } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppComponent } from './app.component';
-import { MediaItemComponent } from './media-item.component';
-import { MediaItemListComponent } from './media-item-list.component';
+import { AppModule } from './app.module';
 
-let appDirectives = [
-  MediaItemComponent,
-  MediaItemListComponent
-];
-
-bootstrap(AppComponent, [
-  {
-    provide: PLATFORM_DIRECTIVES,
-    useValue: appDirectives,
-    multi: true
-  }
-]);
+platformBrowserDynamic().bootstrapModule(AppModule);
