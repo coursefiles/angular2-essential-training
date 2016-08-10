61e1b674f89cc5f6ca7b6a7e38f2fe02ceda951a
Justin Schwartzenberger <schwarty@gmail.com>
2016-08-09 21:49:33 -0700

    update for RC5

--- app/main.ts
+++ app/main.ts
@@ -1,37 +1,5 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { PLATFORM_DIRECTIVES, PLATFORM_PIPES } from '@angular/core';
-import { REACTIVE_FORM_DIRECTIVES, disableDeprecatedForms, provideForms } from '@angular/forms';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppComponent } from './app.component';
-import { MediaItemComponent } from './media-item.component';
-import { MediaItemListComponent } from './media-item-list.component';
-import { MediaItemFormComponent } from './media-item-form.component';
-import { FavoriteDirective } from './favorite.directive';
-import { CategoryListPipe } from './category-list.pipe';
+import { AppModule } from './app.module';
 
-let appDirectives = [
-  MediaItemComponent,
-  MediaItemListComponent,
-  MediaItemFormComponent,
-  FavoriteDirective,
-  REACTIVE_FORM_DIRECTIVES
-];
-
-let appPipes = [
-  CategoryListPipe
-];
-
-bootstrap(AppComponent, [
-  disableDeprecatedForms(),
-  provideForms(),
-  {
-    provide: PLATFORM_DIRECTIVES,
-    useValue: appDirectives,
-    multi: true
-  },
-  {
-    provide: PLATFORM_PIPES,
-    useValue: appPipes,
-    multi: true
-  }
-]);
+platformBrowserDynamic().bootstrapModule(AppModule);
