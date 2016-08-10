bdc3cf500f2536f47a65fe1a5077f13dc3b2302d
Justin Schwartzenberger <schwarty@gmail.com>
2016-08-09 21:59:25 -0700

    fix spacing

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -2,37 +2,37 @@ import { Component } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
-    selector: 'media-item-form',
-    templateUrl: 'app/media-item-form.component.html',
-    styleUrls: ['app/media-item-form.component.css']
+  selector: 'media-item-form',
+  templateUrl: 'app/media-item-form.component.html',
+  styleUrls: ['app/media-item-form.component.css']
 })
 export class MediaItemFormComponent {
-    form: FormGroup;
-    
-    constructor(private formBuilder: FormBuilder) {}
+  form: FormGroup;
 
-    ngOnInit() {
-        this.form = this.formBuilder.group({
-            'medium': this.formBuilder.control('Movies'),
-            'name': this.formBuilder.control('', Validators.compose([
-                Validators.required, 
-                Validators.pattern('[\\w\\-\\s\\/]+')
-                ])),
-            'category': this.formBuilder.control(''),
-            'year': this.formBuilder.control('', this.yearValidator)
-        });
-    }
-    
-    yearValidator(control) {
-        if (control.value.trim().length === 0) return null;
-        var year = parseInt(control.value);
-        var minYear = 1800;
-        var maxYear = 2500;
-        if (year >= minYear && year <= maxYear) return null;
-        return {'year': { 'min': minYear, 'max': maxYear }};
-    }
+  constructor(private formBuilder: FormBuilder) { }
 
-    onSubmit(mediaItem) {
-        console.log(mediaItem);
-    }
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      'medium': this.formBuilder.control('Movies'),
+      'name': this.formBuilder.control('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
+      'category': this.formBuilder.control(''),
+      'year': this.formBuilder.control('', this.yearValidator)
+    });
+  }
+
+  yearValidator(control) {
+    if (control.value.trim().length === 0) return null;
+    var year = parseInt(control.value);
+    var minYear = 1800;
+    var maxYear = 2500;
+    if (year >= minYear && year <= maxYear) return null;
+    return { 'year': { 'min': minYear, 'max': maxYear } };
+  }
+
+  onSubmit(mediaItem) {
+    console.log(mediaItem);
+  }
 }
--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -1,18 +1,15 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
-import { FavoriteDirective } from './favorite.directive';
-
 @Component({
-    selector: 'media-item',
-    directives: [FavoriteDirective],
-    templateUrl: 'app/media-item.component.html',
-    styleUrls: ['app/media-item.component.css']
+  selector: 'media-item',
+  templateUrl: 'app/media-item.component.html',
+  styleUrls: ['app/media-item.component.css']
 })
 export class MediaItemComponent {
-    @Input('mediaItemToWatch') mediaItem;
-    @Output('deleted') delete = new EventEmitter();
-    
-    onDelete() {
-        this.delete.emit(this.mediaItem);
-    }
+  @Input('mediaItemToWatch') mediaItem;
+  @Output('deleted') delete = new EventEmitter();
+
+  onDelete() {
+    this.delete.emit(this.mediaItem);
+  }
 }
