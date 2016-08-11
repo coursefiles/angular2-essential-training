658d4944210b6853a11c76ddd5b98a48f7a3054e
Justin Schwartzenberger <schwarty@gmail.com>
2016-08-10 22:45:21 -0700

    remove early edition of service use

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
-import { MediaItemService } from './media-item.service';
-
 @Component({
   selector: 'media-item-form',
   templateUrl: 'app/media-item-form.component.html',
@@ -11,9 +9,7 @@ import { MediaItemService } from './media-item.service';
 export class MediaItemFormComponent {
   form;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private mediaItemService: MediaItemService) { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.form = this.formBuilder.group({
@@ -37,6 +33,6 @@ export class MediaItemFormComponent {
   }
 
   onSubmit(mediaItem) {
-    this.mediaItemService.add(mediaItem);
+    console.log(mediaItem);
   }
 }
--- app/media-item-list.component.ts
+++ app/media-item-list.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
 
-import { MediaItemService } from './media-item.service';
-
 @Component({
   selector: 'media-item-list',
   templateUrl: 'app/media-item-list.component.html',
   styleUrls: ['app/media-item-list.component.css']
 })
 export class MediaItemListComponent {
-  constructor(private mediaItemService: MediaItemService) { }
 
-  ngOnInit() {
-    this.mediaItems = this.mediaItemService.get();
-  }
-
-  onMediaItemDeleted(mediaItem) {
-    this.mediaItemService.delete(mediaItem);
-  }
+  onMediaItemDeleted(mediaItem) { }
 
   mediaItems = [
     {
