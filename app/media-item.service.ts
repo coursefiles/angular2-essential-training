66b6297298dc0203a61b468d68b2667e3e110446
Justin Schwartzenberger <schwarty@gmail.com>
2016-08-12 12:10:55 -0700

    fix spacing

--- app/media-item.service.ts
+++ app/media-item.service.ts
@@ -4,68 +4,68 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class MediaItemService {
-    constructor(private http: Http) {}
-    
-    get() {
-        return this.http.get('mediaitems')
-            .map(response => {
-                return response.json().mediaItems;
-            });
+  constructor(private http: Http) { }
+
+  get() {
+    return this.http.get('mediaitems')
+      .map(response => {
+        return response.json().mediaItems;
+      });
+  }
+
+  add(mediaItem) {
+    this.mediaItems.push(mediaItem);
+  }
+
+  delete(mediaItem) {
+    var index = this.mediaItems.indexOf(mediaItem);
+    if (index >= 0) {
+      this.mediaItems.splice(index, 1);
     }
-    
-    add(mediaItem) {
-        this.mediaItems.push(mediaItem);
+  }
+
+  mediaItems = [
+    {
+      id: 1,
+      name: "Firebug",
+      medium: "Series",
+      category: "Science Fiction",
+      year: 2010,
+      watchedOn: 1294166565384,
+      isFavorite: false
+    },
+    {
+      id: 2,
+      name: "The Small Tall",
+      medium: "Movies",
+      category: "Comedy",
+      year: 2015,
+      watchedOn: null,
+      isFavorite: true
+    }, {
+      id: 3,
+      name: "The Redemption",
+      medium: "Movies",
+      category: "Action",
+      year: 2016,
+      watchedOn: null,
+      isFavorite: false
+    }, {
+      id: 4,
+      name: "Hoopers",
+      medium: "Series",
+      category: "Drama",
+      year: null,
+      watchedOn: null,
+      isFavorite: true
+    }, {
+      id: 5,
+      name: "Happy Joe: Cheery Road",
+      medium: "Movies",
+      category: "Action",
+      year: 2015,
+      watchedOn: 1457166565384,
+      isFavorite: false
     }
-    
-    delete(mediaItem) {
-        var index = this.mediaItems.indexOf(mediaItem);
-        if (index >= 0) {
-            this.mediaItems.splice(index, 1);
-        }
-    }
-    
-    mediaItems = [
-        {
-            id: 1,
-            name: "Firebug",
-            medium: "Series",
-            category: "Science Fiction",
-            year: 2010,
-            watchedOn: 1294166565384,
-            isFavorite: false
-        },
-        {
-            id: 2,
-            name: "The Small Tall",
-            medium: "Movies",
-            category: "Comedy",
-            year: 2015,
-            watchedOn: null,
-            isFavorite: true
-        }, {
-            id: 3,
-            name: "The Redemption",
-            medium: "Movies",
-            category: "Action",
-            year: 2016,
-            watchedOn: null,
-            isFavorite: false
-        }, {
-            id: 4,
-            name: "Hoopers",
-            medium: "Series",
-            category: "Drama",
-            year: null,
-            watchedOn: null,
-            isFavorite: true
-        }, {
-            id: 5,
-            name: "Happy Joe: Cheery Road",
-            medium: "Movies",
-            category: "Action",
-            year: 2015,
-            watchedOn: 1457166565384,
-            isFavorite: false
-        }
-    ];
+  ];
 }
