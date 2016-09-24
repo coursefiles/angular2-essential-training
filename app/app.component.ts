a149691644fb7b052c895337ea66c3a993d2f491
Justin Schwartzenberger <schwarty@gmail.com>
2016-09-24 11:52:36 -0700

    end of video commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'media-tracker-app',
+  selector: 'mw-app',
   templateUrl: 'app/app.component.html',
   styleUrls: ['app/app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {}
--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'media-item',
+  selector: 'mw-media-item',
   templateUrl: 'app/media-item.component.html',
   styleUrls: ['app/media-item.component.css']
 })
-export class MediaItemComponent {
-  name = 'The Redemption';
-
-  wasWatched () {
-    return true;
-  }
-}
+export class MediaItemComponent { }
