c9bf6d431b8766f8a512fbbdd76a49365ab26ef6
Justin Schwartzenberger <schwarty@gmail.com>
2016-09-24 11:11:42 -0700

    end of video commit

--- app/media-item.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'media-item',
-  templateUrl: 'app/media-item.component.html',
-  styleUrls: ['app/media-item.component.css']
-})
-export class MediaItemComponent { }
