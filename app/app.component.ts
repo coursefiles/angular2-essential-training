8139f9c8669d880d0d8949fde3535057775f01eb
Justin Schwartzenberger <schwarty@gmail.com>
2016-09-24 16:21:33 -0700

    remove onmediaitemdelete and firstmediaitem

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,18 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: 'app/app.component.html',
   styleUrls: ['app/app.component.css']
 })
-export class AppComponent {
-  onMediaItemDelete(mediaItem) {
-    
-  }
-
-  firstMediaItem = {
-    id: 1,
-    name: "Firebug",
-    medium: "Series",
-    category: "Science Fiction",
-    year: 2010,
-    watchedOn: null,
-    isFavorite: false
-  };
-}
+export class AppComponent { }
