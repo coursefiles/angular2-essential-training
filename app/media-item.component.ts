f7e8989ce7050514dd7765f39d3fe2bca6d8d457
Justin Schwartzenberger <schwarty@gmail.com>
2016-09-24 12:38:25 -0700

    end of video commit

--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: 'app/media-item.component.html',
   styleUrls: ['app/media-item.component.css']
 })
-export class MediaItemComponent { }
+export class MediaItemComponent {
+  onDelete() {
+    console.log('deleted');
+  }
+}
