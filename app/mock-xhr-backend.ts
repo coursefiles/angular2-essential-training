d5ee7c067d61a77d0602e7b0c4fef5e01eadaf34
Justin Schwartzenberger <schwarty@gmail.com>
2016-09-24 22:26:11 -0700

    add missing file

---
+++ app/mock-xhr-backend.ts
@@ -0,0 +1,118 @@
+import { Request, Response, ResponseOptions, RequestMethod } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+
+export class MockXHRBackend {
+  constructor() {
+  }
+
+  createConnection(request: Request) {
+    var response = new Observable((responseObserver: Observer<Response>) => {
+      var responseData;
+      var responseOptions;
+      switch (request.method) {
+        case RequestMethod.Get:
+          if (request.url.indexOf('mediaitems?medium=') >= 0 || request.url === 'mediaitems') {
+            var medium;
+            if (request.url.indexOf('?') >= 0) {
+              medium = request.url.split('=')[1];
+              if (medium === 'undefined') medium = '';
+            }
+            var mediaItems;
+            if (medium) {
+              mediaItems = this._mediaItems.filter(mediaItem => mediaItem.medium === medium);
+            } else {
+              mediaItems = this._mediaItems;
+            }
+            responseOptions = new ResponseOptions({
+              body: { mediaItems: JSON.parse(JSON.stringify(mediaItems)) },
+              status: 200
+            });
+          } else {
+            var id = parseInt(request.url.split('/')[1]);
+            mediaItems = this._mediaItems.filter(mediaItem => mediaItem.id === id);
+            responseOptions = new ResponseOptions({
+              body: JSON.parse(JSON.stringify(mediaItems[0])),
+              status: 200
+            });
+          }
+          break;
+        case RequestMethod.Post:
+          var mediaItem = JSON.parse(request.text().toString());
+          mediaItem.id = this._getNewId();
+          this._mediaItems.push(mediaItem);
+          responseOptions = new ResponseOptions({ status: 201 });
+          break;
+        case RequestMethod.Delete:
+          var id = parseInt(request.url.split('/')[1]);
+          this._deleteMediaItem(id);
+          responseOptions = new ResponseOptions({ status: 200 });
+      }
+
+      var responseObject = new Response(responseOptions);
+      responseObserver.next(responseObject);
+      responseObserver.complete();
+      return () => { };
+    });
+    return { response };
+  }
+
+  _deleteMediaItem(id) {
+    var mediaItem = this._mediaItems.find(mediaItem => mediaItem.id === id);
+    var index = this._mediaItems.indexOf(mediaItem);
+    if (index >= 0) {
+      this._mediaItems.splice(index, 1);
+    }
+  }
+
+  _getNewId() {
+    if (this._mediaItems.length > 0) {
+      return Math.max.apply(Math, this._mediaItems.map(mediaItem => mediaItem.id)) + 1;
+    }
+  }
+
+  _mediaItems = [
+    {
+      id: 1,
+      name: "Firebug",
+      medium: "Series",
+      category: "Science Fiction",
+      year: 2010,
+      watchedOn: 1294166565384,
+      isFavorite: false
+    },
+    {
+      id: 2,
+      name: "The Small Tall",
+      medium: "Movies",
+      category: "Comedy",
+      year: 2015,
+      watchedOn: null,
+      isFavorite: true
+    }, {
+      id: 3,
+      name: "The Redemption",
+      medium: "Movies",
+      category: "Action",
+      year: 2016,
+      watchedOn: null,
+      isFavorite: false
+    }, {
+      id: 4,
+      name: "Hoopers",
+      medium: "Series",
+      category: "Drama",
+      year: null,
+      watchedOn: null,
+      isFavorite: true
+    }, {
+      id: 5,
+      name: "Happy Joe: Cheery Road",
+      medium: "Movies",
+      category: "Action",
+      year: 2015,
+      watchedOn: 1457166565384,
+      isFavorite: false
+    }
+  ];
+}
