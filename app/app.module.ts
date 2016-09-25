61793871b1373772ff19603ee338d86e0938ca5a
Justin Schwartzenberger <schwarty@gmail.com>
2016-09-24 20:26:14 -0700

    end of video commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,12 +5,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
-import { MediaItemFormComponent } from './media-item-form.component';
 import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
+import { MediaItemFormComponent } from './media-item-form.component';
 import { MediaItemService } from './media-item.service';
 
-let lookupLists = {
+const lookupLists = {
   mediums: ['Movies', 'Series']
 };
 
@@ -23,14 +23,16 @@ let lookupLists = {
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
-    MediaItemFormComponent,
     FavoriteDirective,
-    CategoryListPipe
+    CategoryListPipe,
+    MediaItemFormComponent
   ],
   providers: [
     MediaItemService,
     { provide: 'lookupListToken', useValue: lookupLists }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {}
