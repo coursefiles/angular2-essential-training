cca01e2b8a1de38a09d52267760f4d61ffdd53bb
Justin Schwartzenberger <schwarty@gmail.com>
2016-09-24 17:19:42 -0700

    end of video commit

--- app/favorite.directive.ts
+++ app/favorite.directive.ts
@@ -5,7 +5,6 @@ import { Directive, HostBinding, Input } from '@angular/core';
 })
 export class FavoriteDirective {
   @HostBinding('class.is-favorite') isFavorite = true;
-
   @Input() set mwFavorite(value) {
     this.isFavorite = value;
   }
