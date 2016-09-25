d0e0033d694af88955d72da46aff5c1c516fd45b
Justin Schwartzenberger <schwarty@gmail.com>
2016-09-24 19:10:07 -0700

    end of video commit

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'media-item-form',
+  selector: 'mw-media-item-form',
   templateUrl: 'app/media-item-form.component.html',
   styleUrls: ['app/media-item-form.component.css']
 })
@@ -17,19 +17,21 @@ export class MediaItemFormComponent {
         Validators.pattern('[\\w\\-\\s\\/]+')
       ])),
       category: new FormControl(''),
-      year: new FormControl('', this.yearValidator)
+      year: new FormControl('', this.yearValidator),
     });
   }
 
   yearValidator(control) {
-    if (control.value.trim().length === 0) return null;
+    if(control.value.trim().length === 0) {
+      return null;
+    }
     let year = parseInt(control.value);
     let minYear = 1900;
     let maxYear = 2100;
-    if (year >= minYear && year <= maxYear) {
+    if(year >= minYear && year <= maxYear) {
       return null;
     } else {
-      return { year: true };
+      return { 'year': true };
     }
   }
 
