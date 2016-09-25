8de4b202f5fbf6cedd0733fa13fdbd09a5821119
Justin Schwartzenberger <schwarty@gmail.com>
2016-09-25 15:48:40 -0700

    fix systemjs and route entries

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -5,6 +5,6 @@ import { MediaItemListComponent } from './media-item-list.component';
 
 const appRoutes: Routes = [
   { path: 'add', component: MediaItemFormComponent },
-  { path: ':medium', component: MediaItemFormComponent },
-  { path: '', redirectTo: 'all' }
+  { path: ':medium', component: MediaItemListComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'all' }
 ];
