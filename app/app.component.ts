215346d75aef8a5b849cdc286c72af5207461eaf
Justin Schwartzenberger <schwarty@gmail.com>
2016-09-24 19:50:37 -0700

    end of video commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'media-tracker-app',
+  selector: 'mw-app',
   templateUrl: 'app/app.component.html',
   styleUrls: ['app/app.component.css']
 })
