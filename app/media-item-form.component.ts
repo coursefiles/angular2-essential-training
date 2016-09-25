aec226d90507102beb64d88dad3e8300178d0ce2
Justin Schwartzenberger <schwarty@gmail.com>
2016-09-24 19:01:21 -0700

    end of video commit

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'media-item-form',
+  selector: 'mw-media-item-form',
   templateUrl: 'app/media-item-form.component.html',
   styleUrls: ['app/media-item-form.component.css']
 })
@@ -17,7 +17,7 @@ export class MediaItemFormComponent {
         Validators.pattern('[\\w\\-\\s\\/]+')
       ])),
       category: new FormControl(''),
-      year: new FormControl('')
+      year: new FormControl(''),
     });
   }
 
