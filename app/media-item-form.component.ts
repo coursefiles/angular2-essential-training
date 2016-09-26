2b73fca490415c6e89c59977cd7f0e8271573465
Justin Schwartzenberger <schwarty@gmail.com>
2016-09-25 17:55:12 -0700

    final video commit

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { MediaItemService } from './media-item.service';
 import { lookupListToken } from './providers';
@@ -15,7 +16,8 @@ export class MediaItemFormComponent {
   constructor(
     private formBuilder: FormBuilder,
     private mediaItemService: MediaItemService,
-    @Inject(lookupListToken) public lookupLists) {}
+    @Inject(lookupListToken) public lookupLists,
+    private router: Router) {}
 
   ngOnInit() {
     this.form = this.formBuilder.group({
@@ -50,6 +52,8 @@ export class MediaItemFormComponent {
 
   onSubmit(mediaItem) {
     this.mediaItemService.add(mediaItem)
-      .subscribe();
+      .subscribe(() => {
+        this.router.navigate(['/', mediaItem.medium]);
+      });
   }
 }
