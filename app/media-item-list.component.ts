b2541adaa4e899d454fbd6da162ad269455eb7a0
Justin Schwartzenberger <schwarty@gmail.com>
2016-09-25 17:28:03 -0700

    end of video commit

--- app/media-item-list.component.ts
+++ app/media-item-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { MediaItemService } from './media-item.service';
 
@@ -11,10 +12,19 @@ export class MediaItemListComponent {
   medium = '';
   mediaItems = [];
 
-  constructor(private mediaItemService: MediaItemService) {}
+  constructor(
+    private mediaItemService: MediaItemService,
+    private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
-    this.getMediaItems(this.medium);
+    this.activatedRoute.params
+      .subscribe(params => {
+        let medium = params['medium'];
+        if(medium.toLowerCase() === 'all') {
+          medium = '';
+        }
+        this.getMediaItems(medium);
+      });
   }
 
   onMediaItemDelete(mediaItem) {
