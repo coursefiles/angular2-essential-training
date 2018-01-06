a85c40965c4c4d16d1606fbcd05f231536015f2d
Justin Schwartzenberger <schwarty@gmail.com>
2018-01-06 14:42:32 -0800

    update MockXhrBackend for Angular 5.0.0
    (cherry picked from commit f76ada1)

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpModule, XHRBackend } from '@angular/http';
+import { HttpClientModule, HttpXhrBackend } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
@@ -17,7 +17,7 @@ import { MockXHRBackend } from './mock-xhr-backend';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    HttpModule
+    HttpClientModule
   ],
   declarations: [
     AppComponent,
@@ -30,7 +30,7 @@ import { MockXHRBackend } from './mock-xhr-backend';
   providers: [
     MediaItemService,
     { provide: lookupListToken, useValue: lookupLists },
-    { provide: XHRBackend, useClass: MockXHRBackend }
+    { provide: HttpXhrBackend, useClass: MockXHRBackend }
   ],
   bootstrap: [
     AppComponent
