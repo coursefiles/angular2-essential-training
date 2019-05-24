8b0a110483837de6047eac904e61dd9fd70aae9d
Hurlyburly2 <dougdougmann@gmail.com>
2019-05-24 17:02:31 -0400

    continuing lesson

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,1 +1,12 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [],
+  bootstrap: []
+})
+export class AppModule {}
